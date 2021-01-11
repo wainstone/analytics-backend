f23c92f42cdb9000c15f823cf19e1b10a6c2756e
Ben Harder <ben.harder2@gmail.com>
2021-01-11 11:49:45 -0800

    added env distinction between stacks and some resourses, general improvements

--- cloudformation/deploy.py
+++ cloudformation/deploy.py
@@ -1,5 +1,7 @@
 import boto3 
 import os
+import argparse
+from time import sleep
 
 
 def _parse_template(template, cloudformation):
@@ -8,20 +10,53 @@ def _parse_template(template, cloudformation):
     cloudformation.validate_template(TemplateBody=template_data)
     return template_data
 
+def watchStack(cf_client, stackName):
+    while True:
+        sleep(2)
+
+        stacks = cf_client.describe_stacks(StackName=stackName)['Stacks']
+        
+        if len(stacks) == 0:
+            print("Stack with name " + stackName + " not found. Retrying...")
+        else:
+            stack = stacks[0]
+            status = stack['StackStatus']
+            
+            if ('COMPLETE' or 'FAILED') not in status:
+                print("Stack creation in progress. Status: " + status)
+            else:
+                print("Stack creation finished with status: " + status)
+                break
 
 def main():
-    stackName = 'athlytics-jy75'
-    cloudformation = boto3.client('cloudformation')
+
+    parser = argparse.ArgumentParser(description='Process deployment arguments.')
+    parser.add_argument('--env', type=str, default="prod", help='The environment to deploy the stack to.')
+    args = parser.parse_args()
+
+    stackName = 'athlytics-jy75-' + args.env
+    cf_client = boto3.client('cloudformation')
     
-    templateBody = _parse_template(os.getcwd() + "/cf_backend.yaml", cloudformation)
+    templateBody = _parse_template(os.getcwd() + "/cf_backend.yaml", cf_client)
 
     print('Attempting to create stack...')
     try: 
-        cloudformation.create_stack(StackName=stackName, TemplateBody=templateBody, Capabilities=['CAPABILITY_IAM'])
+        cf_client.create_stack(StackName=stackName, TemplateBody=templateBody, Capabilities=['CAPABILITY_IAM'], Parameters=[{"ParameterKey": "Environment", "ParameterValue": args.env}])
+        watchStack(cf_client, stackName)
     except Exception as e:
         if 'already exists' in str(e):
             print('The stack already existed. Updating...')
-            cloudformation.update_stack(StackName=stackName, TemplateBody=templateBody, Capabilities=['CAPABILITY_IAM'])            
+            try:
+                cf_client.update_stack(StackName=stackName, TemplateBody=templateBody, Capabilities=['CAPABILITY_IAM'], Parameters=[{"ParameterKey": "Environment", "ParameterValue": args.env}])   
+                watchStack(cf_client, stackName)  
+            except Exception as e:
+                if 'No updates are to be performed' in str(e):
+                    print("The provided template is the same; no updates were performed.")
+                else:
+                    print(e)
+        else:
+            print(e)
+                   
 
 if __name__ == "__main__":
     main()
