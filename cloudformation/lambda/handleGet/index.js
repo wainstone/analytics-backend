812a5ba68e5dc84383bbdbbe3a84185ded3ae99c
Ben Harder <ben.harder2@gmail.com>
2021-02-18 14:40:56 -0800

    find similar working!

--- cloudformation/lambda/handleGet/index.js
+++ cloudformation/lambda/handleGet/index.js
@@ -1,6 +1,7 @@
 const AWS = require('aws-sdk');
 
 const dynamo = new AWS.DynamoDB.DocumentClient();
+const TABLENAME = "athlytics_jy75-options" // this should be converted to an env variable 
 
 exports.handler = async (event, context) => {
     let body;
@@ -11,19 +12,21 @@ exports.handler = async (event, context) => {
         "Access-Control-Allow-Credentials" : true
     };
     let command = parseInt(event.headers.command)
-    console.log(command)
     try {
         switch (command) {
             case 1:
-                body = "FindSimilar called";
+                let percentiles = event.headers.percentiles.split(",").map(x => parseFloat(x));
+                let threshold = event.headers.threshold;
+                let data = await dynamo.scan({ TableName: TABLENAME }).promise();
+                body = meanSquareError(data["Items"], percentiles, threshold);
                 break;
             default:
+                statusCode = '400';
                 throw new Error(`Unsupported method "${event.httpMethod}"`);
         }
 
         
     } catch (err) {
-        statusCode = '400';
         body = err.message;
     } finally {
         body = JSON.stringify(body);
@@ -35,30 +38,31 @@ exports.handler = async (event, context) => {
     };
 };
 
-function meanSquareError(athletes, placements, threshold) {
+function meanSquareError(athletes, percentiles, threshold) {
     let similar = [];
-    for (const [athlete, races] of Object.entries(athletes)) {
+    athletes.forEach(entry => {
+        let athlete = entry["athlete"];
+        let races = entry["races"];
 
         let hsRaces = races.filter(race => race.category != "university");
-
-        if (hsRaces.length < placements.length) {
-            continue;
+        console.log(hsRaces.length);
+        console.log(percentiles.length);
+        if (hsRaces.length >= percentiles.length) {
+            let length = Math.min(hsRaces.length, percentiles.length);
+            let sum = 0;
+            for (let i = 0; i < length; i++) {
+                sum += Math.pow((hsRaces[i].percentile - percentiles[i]), 2)
+            }
+            let mse = sum / length;
+            
+            similar.push({ 'athlete': athlete, 'races': races, 'mse': mse});
         }
-
-        length = Math.min(hsRaces.length, placements.length);
-        let sum = 0;
-        for (i = 0; i < length; i++) {
-            sum += Math.pow((hsRaces[i].place - placements[i]), 2)
-        }
-        let mse = sum / length;
-        
-        similar.push({'athlete': athlete, 'mse': mse});
-    }
+    });
     similar.sort((a, b) => a.mse - b.mse);
 
-    topAthletes = {};
-    for (i = 0; i < Math.min(threshold, similar.length); i++) {
-        topAthletes[similar[i].athlete] = athletes[similar[i].athlete];
+    let topAthletes = [];
+    for (let i = 0; i < Math.min(threshold, similar.length); i++) {
+        topAthletes.push(similar[i]);
     }
     return topAthletes;
 }
