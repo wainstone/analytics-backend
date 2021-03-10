e220733857ae7565c44232efb8a58c14a03f614c
Ben Harder <ben.harder2@gmail.com>
2021-03-09 18:21:42 -0800

    added second algorithm and some basic error checking

--- cloudformation/lambda/handleGet/index.js
+++ cloudformation/lambda/handleGet/index.js
@@ -52,10 +52,16 @@ exports.handler = async (event, context) => {
         let command = parseInt(event.queryStringParameters.command);
         switch (command) {
             case 1:
-                let percentiles = event.queryStringParameters.percentiles.split(",").map(x => parseFloat(x));
-                let threshold = event.queryStringParameters.threshold;
-                let data = await dynamo.scan({ TableName: TABLENAME }).promise();
+                var percentiles = event.queryStringParameters.percentiles.split(",").map(x => parseFloat(x));
+                var threshold = event.queryStringParameters.threshold;
+                var data = await dynamo.scan({ TableName: TABLENAME }).promise();
                 body = meanSquareError(data["Items"], percentiles, threshold);
+                break;
+            case 2: 
+                var percentiles = event.queryStringParameters.percentiles.split(",").map(x => parseFloat(x));
+                var numAthletes = event.queryStringParameters.threshold;
+                var data = await dynamo.scan({ TableName: TABLENAME }).promise();
+                body = findPlacers(data["Items"], percentiles, 5, numAthletes);
                 break;
             case 3:
                 var params = { TableName: TABLENAME };
@@ -91,7 +97,7 @@ exports.handler = async (event, context) => {
 
         
     } catch (err) {
-        body = err.message;
+        body = err;
     } finally {
         body = JSON.stringify(body);
     }
@@ -102,7 +108,84 @@ exports.handler = async (event, context) => {
     };
 };
 
+function findPlacers(athletes, percentiles, threshold, numAthletes) {
+    let seeds = {}; // Uni athletes who passed the placement test. Used to get eligible athletes.
+    if (percentiles.length != 2) {
+        throw "list of percentiles is not length 2"
+    }
+    if (threshold == undefined) {
+        throw "threshold not given"
+    }
+    if (numAthletes == undefined) {
+        throw "numAthletes not given"
+    }
+    let low = Math.min(...percentiles);
+    let high = Math.max(...percentiles);
+    if (high > 1) {
+        throw "max percentile was above 1. Percentiles should be between 0 and 1"
+    }
+
+    athletes.forEach(entry => {
+        let athlete = entry["athlete"];
+        let races = entry["races"];
+        let uniRaces = races.filter(race => race.category == "university");
+        let good = false;
+
+        for (let i = 0; i < uniRaces.length; i++) {
+            let race = uniRaces[i];
+            if (low <= race.percentile && race.percentile <= high) { // Maybe add MSE here compared against placements in the range to prioritize athletes who land more in the range. Multiply the MSE of an athlete to the middle of the range with a seed athlete's similar athlete result MSEs
+                good = true;
+                break;
+            }
+        }
+        // Seed athlete should have at least 3 highschool races for good similarity comparisons.
+        if (good && 3 <= races.filter(race => race.category != "university").length) {
+            seeds[athlete] = races;
+        }
+    });
+    
+    let intermediate = [] // all athletes returned from the similarity algorithm for each seed athlete
+    for (const athlete in seeds) {
+        let races = seeds[athlete];
+        let hsRaces = races.filter(race => race.category != "university");
+        let hsPlacements = hsRaces.map(x => x.percentile);
+        intermediate.push(...meanSquareError(athletes, hsPlacements, threshold)); // Push all similar athletes onto the list. Will contain duplicates (but with different MSE's)
+    }
+    // Since intermediate might contain duplicate athletes (because two seed athletes could find the same athlete as similar), average their MSE's
+    let averaged = averageMses(intermediate);
+    averaged.sort((a,b) => a.mse - b.mse);
+    return averaged.slice(0, numAthletes) // Let the frontend do the rest of the filtering on this set
+}
+
+// Helper function for findPlacers. Returns the averaged MSEs of the intermediate list of similar athletes.
+function averageMses(intermediate) {
+    let unique = {};
+    let averagedList = []; // contains list of unique athletes with averaged MSEs.
+    let copy = [...intermediate];
+
+    copy.forEach((entry) => {
+        if (entry.athlete in unique) {
+            unique[entry.athlete].mses.push(entry.mse);
+        }
+        else {
+            unique[entry.athlete] = entry;
+            unique[entry.athlete].mses = [entry.mse]; // add field to hold list of mses
+        }
+    });
+    Object.values(unique).forEach(entry => {
+        averagedList.push({'athlete': entry.athlete, 'races': entry.races, 'mse': (entry.mses.reduce((a, b) => a + b) / entry.mses.length)})
+    })
+    
+    return averagedList;
+}
+
 function meanSquareError(athletes, percentiles, threshold) {
+    if (percentiles == undefined) {
+        throw "percentiles not given";
+    }
+    if (threshold == undefined) {
+        throw "threshold not given";
+    }
     let similar = [];
     athletes.forEach(entry => {
         let athlete = entry["athlete"];
